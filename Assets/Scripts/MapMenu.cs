8cb3cff9b3349540205c4afd0cea68235470caae
Unknown <Kboi0@live.com>
2019-01-26 01:23:21 -0600

    Adds Scene Change Scripts

---
+++ Assets/Scripts/MapMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OptionsMenu() //assign this to the "start" button
+    {
+        Debug.Log("Options!"); 
+        SceneManager.LoadScene("test1");
+    }
+  public void Victorian() //assign to Victorian House
+    {
+        Debug.Log("Victorian!");
+        SceneManager.LoadScene("test2");
+    }
+    public void Cottage() //assign to Cottage
+    {
+        Debug.Log("Cottage!");
+        SceneManager.LoadScene("test3");
+    }
+    public void Mansion() //assign to Mansion
+    {
+        Debug.Log("Mansion!");
+        SceneManager.LoadScene("test4");
+    }
+}
