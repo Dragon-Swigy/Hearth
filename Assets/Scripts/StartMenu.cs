c6bd202423251fe7c3f1527d92a693f2e02bcfd1
Unknown <Kboi0@live.com>
2019-01-26 00:31:03 -0600

    Adds Map Screen, Start Menu Scripts

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame () //assign this to the "start" button
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame() //assign this to the "log out" button
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+
+    }
+}
