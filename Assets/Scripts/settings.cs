05bd8cadb85cb71e6ee5ec96f9c39fa4614dafbc
Unknown <luis777_hdz_11@hotmail.com>
2019-01-26 14:34:23 -0600

    Credit Screen
    The screen is done I think

--- Assets/Scripts/settings.cs
+++ Assets/Scripts/settings.cs
@@ -15,7 +15,7 @@ public class settings : MonoBehaviour
 
     public void Return() //assign this to the "start" button
     {
-        SceneManager.LoadScene("MapScreen");
+        SceneManager.LoadScene("StartMenu");
     }
 
     public void QuitGame() //assign this to the "log out" button
