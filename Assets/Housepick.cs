0a18e9613faed4c6daad547a7a6f1767b80da350
Unknown <Kboi0@live.com>
2019-01-27 10:55:34 -0600

    Adds End Scenes

---
+++ Assets/Housepick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Housepick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void buyApartment()
+    { 
+        Debug.Log("Options!");
+        SceneManager.LoadScene("ApartmentEnd");
+    }
+    public void buyCottage() 
+    {
+        Debug.Log("Options!");
+        SceneManager.LoadScene("CottageEnd");
+    }
+    public void buyMansion()
+    {
+        Debug.Log("Options!");
+        SceneManager.LoadScene("MansionEnd");
+    }
+    public void buyTraphouse() 
+    {
+        Debug.Log("Options!");
+        SceneManager.LoadScene("TrapEnd");
+    }
+}
