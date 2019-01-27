0765b0953d9bc225f5dcc740eec6eeabf784bc75
Unknown <luis777_hdz_11@hotmail.com>
2019-01-26 21:49:03 -0600

    Push credits over the wall

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -22,5 +22,7 @@ public class Timer : MonoBehaviour
 
         string minutes = ((int)t / 60).ToString();
         string seconds = (t % 60).ToString();
+
+        timerText.text = minutes + ":" + seconds;
     }
 }
